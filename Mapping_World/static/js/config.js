f56a5f215a6f0246446c436110cc98ec96c7cab4
Kat Marcinkowski <95448573+katmarcin@users.noreply.github.com>
2022-05-15 23:28:38 -0400

    HTML/JS for Mapping (work in progress)

---
+++ Mapping_World/static/js/config.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1Ijoia2F0bWFyY2luIiwiYSI6ImNsMGhheTF1YTA2d28za3BuNWNsd2FsaHIifQ.q9Xj_SDv8GMpECTL7azl5w";
---
+++ Mapping_World/static/js/testing-colors.js
@@ -0,0 +1,98 @@
+
+// We create the dark view tile layer that will be an option for our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+	maxZoom: 18,
+	accessToken: API_KEY
+});
+
+// We create the dark view tile layer that will be an option for our map.
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+	maxZoom: 18,
+	accessToken: API_KEY
+});
+
+// Create a base layer that holds both maps.
+let baseMaps = {
+  "Streets": streets,
+  "Satellite Streets": satelliteStreets
+};
+
+// Create the map object with center, zoom level and default layer.
+let map = L.map('mapid', {
+	center: [43.7, -79.3],
+	zoom: 4,
+	layers: [satelliteStreets]
+});
+
+
+///stopped right here (at this point the color of the map dissappeared)
+
+
+
+// Pass our map layers into our layer control and add the layer control to the map.
+L.control.layers(baseMaps).addTo(map);
+
+// Call the data
+
+d3.json("https://raw.githubusercontent.com/camillecoding/project/main/geo_data.json").then(function(data) {
+  console.log(data);
+  // This function returns the style data 
+
+  function styleInfo(feature) {
+    return {
+      opacity: 1,
+      fillOpacity: 1,
+      fillColor: getColor(feature.properties.name),
+      color: "#000000",
+      radius: getRadius(feature.properties.name),
+      stroke: true,
+      weight: 0.5
+    };
+  }
+
+  // This function determines the color of the circle based on co2 emissions
+  function getColor(co2) {
+    if (co2 > 5) {
+      return "#ea2c2c";
+    }
+    if (co2 > 4) {
+      return "#ea822c";
+    }
+    if (co2 > 3) {
+      return "#ee9c00";
+    }
+    if (co2 > 2) {
+      return "#eecc00";
+    }
+    if (co2 > 1) {
+      return "#d4ee00";
+    }
+    return "#98ee00";
+  }
+  
+  // Countries with a CO2 value of 0 were being plotted with the wrong radius. (additional function)
+  function getRadius(co2) {
+    if (co2 === 0) {
+      return 1;
+    }
+    return co2 * 4;
+  }
+
+// Creating a GeoJSON layer with the retrieved data.
+L.geoJson(data, {
+
+  // We turn each feature into a circleMarker on the map.
+  pointToLayer: function(feature, latlng) {
+              console.log(data);
+              return L.circleMarker(latlng);
+          },
+          style: styleInfo,
+    // We create a popup for each circleMarker to display info
+    onEachFeature: function(feature, layer) {
+      layer.bindPopup("Country: " + feature.properties.name + "Year:" + feature.properties.years);
+    }
+  }).addTo(map)
+
+});
